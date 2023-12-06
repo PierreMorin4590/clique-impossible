fa0a47e3357d7d5ca40c41f63af5a394aa3beeca
PierreMorin4590 <pierremorin4590@hotmail.fr>
2023-12-06 13:39:38 +0100

    Ajout des commentaires sur le fichier js

--- js/trick-box.js
+++ js/trick-box.js
@@ -6,27 +6,38 @@ const trickBox = {
 
     bindFirstTrickBox: function() {
         const bindFirstTrickBox = document.querySelector(".trick-box");
+        // On pose l'écouteur sur la bonne div
         bindFirstTrickBox.addEventListener("mouseover", trickBox.handleTrickBox);
     },
 
     handleTrickBox: function(event) {
-        console.log("C'est okkkkkkkkkk !");
+        // Petit test pour voir si la console affiche le message quand la souris arrive dans la div .trick-box
+        console.log("Quand on passe dans .trick-box");
+        // On sélectionne le bouton
         let selectButton = document.querySelector("#button");
         console.log(selectButton);
+        // On supprime le bouton
         selectButton.remove();
+        // Quand la souris sort de la div .trick-box, on fait réapparaître le bouton
         document.querySelector(".trick-box").onmouseout = trickBox.buttonReappear;
     },
 
     buttonReappear: function () {
+        // On créé un élément
         let buttonReappear = document.createElement("button");
-        console.log(buttonReappear);
+        // On lui assigne un texte
         buttonReappear.textContent = "Allez, clique !";
+        // On lui donne une id
         buttonReappear.setAttribute("id", "button");
+        // On lui donne une class
         buttonReappear.setAttribute("class", "button");
+        // On lui donne un type
         buttonReappear.setAttribute("type", "button");
+        // On le place dans la div . trick-box
         document.querySelector(".trick-box").append(buttonReappear);
     },
 
 }
 
+// On charge les fonctions de la méthode init
 document.addEventListener("DOMContentLoaded", trickBox.init);
